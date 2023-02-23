e137dcbb82cdf8947d9e7ccf2767726ee5741cb9
Dmitry F <dimfroloff@gmail.com>
2023-02-23 10:54:02 +0300

    Initial commit

---
+++ src/test/java/com/jsd/qa_guru_examples/SelenideRepositorySearch.java
@@ -0,0 +1,22 @@
+package com.jsd.qa_guru_examples;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideRepositorySearch {
+
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+
+//        открыть главную страницу
+        open("https://github.com/");
+//        ввести в поле поиска selenide и нажать enter
+        $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
+//        кликнуть на первый репозиторий из списка найденных
+        $$("ul.repo-list li").first().$("a").click();
+//        проверка: заголовок selenide/selenide
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+    }
+}
