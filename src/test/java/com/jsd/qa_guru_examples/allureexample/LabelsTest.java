856d74090822f5e788d757f00337c7961eafd2e5
Dmitry F <dimfroloff@gmail.com>
2023-03-23 00:21:23 +0300

    Allure report

---
+++ src/test/java/com/jsd/qa_guru_examples/allureexample/LabelsTest.java
@@ -0,0 +1,41 @@
+package com.jsd.qa_guru_examples.allureexample;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.apache.xmlbeans.impl.xb.xsdschema.All;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class LabelsTest {
+
+    @Test
+    @Feature("Issue в репозитории")
+    @Story("Создание Issue")
+    @Owner("eroshenkoam")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Testing", url = "https://testing.github.com")
+    @DisplayName("Создание Issue для авторизованного пользователя")
+    public void testStaticLabels() {
+
+    }
+
+    @Test
+    public void testDynamicLabels() {
+        Allure.getLifecycle().updateTestCase(
+                t -> t.setName("Создание Issue для авторизованного пользователя")
+        );
+        Allure.feature("Issue в репозитории");
+        Allure.story("Создание Issue");
+        Allure.label("owner", "eroshenkoam");
+        Allure.label("severity", SeverityLevel.CRITICAL.value());
+        Allure.link("Testing", "https://testing.github.com");
+    }
+}
--- src/test/java/com/jsd/qa_guru_examples/allureexample/WebSteps.java
+++ src/test/java/com/jsd/qa_guru_examples/allureexample/WebSteps.java
@@ -1,7 +1,11 @@
 package com.jsd.qa_guru_examples.allureexample;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -35,5 +39,10 @@ public class WebSteps {
     @Step("Проверяем наличие Issue с номером {issue}")
     public void shouldSeeIssueWithNumber(int issue) {
         $(withText("#" + issue)).should(Condition.exist);
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
