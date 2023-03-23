0e264f9700fe0631285498696ef0365e43fac0f7
Dmitry F <dimfroloff@gmail.com>
2023-03-23 20:56:30 +0300

    unused imports remove

--- src/test/java/com/jsd/qa_guru_examples/allureexample/LabelsTest.java
+++ src/test/java/com/jsd/qa_guru_examples/allureexample/LabelsTest.java
@@ -1,18 +1,8 @@
 package com.jsd.qa_guru_examples.allureexample;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.apache.xmlbeans.impl.xb.xsdschema.All;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static io.qameta.allure.Allure.step;
-import static org.openqa.selenium.By.linkText;
 
 public class LabelsTest {
 
